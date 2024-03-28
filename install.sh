943c0bfff23bd6e31fcbea86770ae36e46d5e9f1
Gaurang Shah <gaurang.shah@definity.com>
2024-03-27 21:45:19 -0400

    replaced nvim-tree with neo-tree

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 brew install --cask iterm2
 brew install tmux
 brew install exa
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install neovim
 brew install stow
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
+brew tap homebrew/cask-fonts
+brew install --cask homebrew/cask-fonts/font-0xproto-nerd-font
 brew install ripgrep
 brew install lazygit
 stow --adopt .
