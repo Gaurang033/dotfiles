6058f3eea9a73e4d1eac5016d98ffb1925efb2c7
Gaurang Shah(v) <fki@eco100194m.federation.ca>
2024-04-24 14:45:47 -0400

    modified tmux for ohmyzsh and added theme file

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ dotfiles for macos
 
 * run install.sh 
 * to install tmux plugins `run <C-s> I`
-
+* run `p10k configure` to configure p10 theme
--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@ brew install tmux
 #brew install exa
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+brew install --HEAD luajit
+brew install --HEAD tree-sitter
 brew install neovim
 brew install stow
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 brew tap homebrew/cask-fonts
-brew install --cask homebrew/cask-fonts/font-0xproto-nerd-font
+#brew install --cask homebrew/cask-fonts/font-0xproto-nerd-font
 brew install ripgrep
 brew install lazygit
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
