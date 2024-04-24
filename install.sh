345d2067052255a8b736f8226989a2a0e86a3c40
Gaurang Shah(v) <fki@eco100194m.federation.ca>
2024-04-24 13:49:18 -0400

    fixed install script for m2

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 brew install --cask iterm2
 brew install tmux
-brew install exa
+#brew install exa
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 brew install neovim
 brew install stow
 brew tap hashicorp/tap
@@ -10,5 +11,5 @@ brew tap homebrew/cask-fonts
 brew install --cask homebrew/cask-fonts/font-0xproto-nerd-font
 brew install ripgrep
 brew install lazygit
-qit clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
 stow --adopt .
