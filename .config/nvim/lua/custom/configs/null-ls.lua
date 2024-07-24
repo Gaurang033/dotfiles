ea5f798c6179c3f393fb82db0da2c98f3ac3ef25
Gaurang Shah(v) <fki@eco100194m.local>
2024-07-24 19:10:16 -0400

    added terragrunt formatter

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -2,21 +2,23 @@ local augroup = vim.api.nvim_create_augroup("lspformatting", {})
 local null_ls = require "null-ls"
 
 local opts = {
+  -- debug = true,
+  -- logging_level = "debug",
   sources = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.stylua,
     -- null_ls.builtins.diagnostics.mypy,
     -- null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.terragrunt_fmt,
     null_ls.builtins.formatting.shellharden,
     null_ls.builtins.formatting.prettier.with { filetypes = { "json", "yaml" } },
     null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.hclfmt,
+    -- null_ls.builtins.formatting.hclfmt,
     null_ls.builtins.diagnostics.pylint.with {
       prefer_local = ".venv/bin",
       extra_args = { "--disable=C0114,C0115,C0116" },
     },
-    null_ls.builtins.diagnostics.terraform_validate,
     null_ls.builtins.diagnostics.terraform_validate,
     null_ls.builtins.diagnostics.sqlfluff.with {
       extra_args = { "--dialect", "bigquery" }, -- change to your dialect
