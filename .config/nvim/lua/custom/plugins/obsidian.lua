aca20f7fdce43ff2e073a1296fa3e1b7c96e4483
Gaurang Shah(v) <fki@3a:06:4a:ab:df:f9.federation.ca>
2024-12-25 10:31:18 -0500

    added obsidian plugin

--- .config/nvim/lua/custom/plugins/obsidian.lua
+++ .config/nvim/lua/custom/plugins/obsidian.lua
@@ -14,8 +14,6 @@ return {
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
@@ -25,6 +23,8 @@ return {
       },
     },
 
-    -- see below for full list of options 👇
+    note_id_func = function(title)
+      return title
+    end,
   },
 }
